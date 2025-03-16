964f62f1e5e9cb7eee1007b266a93c6d7bcf6079
fox3000foxy <fox3000foxy@gmail.com>
2025-03-16 13:58:01 +0100

    Update dependencies and modify video playback mode

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const { Readable } = require("stream");
+const {
+    Readable
+} = require("stream");
 const aimlHigh = require('./aiml-high');
 const fs = require('fs');
-const { Hercai } = require('hercai');
+const {
+    Hercai
+} = require('hercai');
 const SearchEngine = require('cdrake-se');
 const https = require('https');
 
-process.env = {...process.env, ...require('dotenv').config().parsed} 
+process.env = {
+    ...process.env,
+    ...require('dotenv').config().parsed
+}
+// console.log(process.env)
 
 // Constants
 const PORT = 3000;
@@ -32,7 +40,10 @@ const botProperties = require(BOT_PROPERTIES_PATH);
 const datasets = require(DATASETS_PATH);
 
 const app = express();
-const herc = new Hercai(HERC_API_KEY); // Initialize Hercai with API key
+const herc = new Hercai({
+    apiKey: ""
+}); // Initialize Hercai with API key
+// console.log("Loaded data", herc);
 const interpreter = new aimlHigh(botProperties, 'Goodbye');
 
 // Load AIML datasets
@@ -43,34 +54,35 @@ app.use(bodyParser.json());
 
 // Serve the index page
 app.get('/', (req, res) => {
+    console.log("Serving index page");
     res.sendFile(INDEX_FILE);
 });
 
 // Helper functions
 function generatePrompt(question) {
     let promptTemplate = fs.readFileSync(`${DATA_DIR}/prompt.txt`).toString();
-	if(question.includes("météo")) {
-		promptTemplate += `Voici la météo pour les 7 prochains jours: \n${format7DayForecast(getMeteoCache())}. Ne donne que celle qui t'es demandé.`
-	}
-	if(question.includes("température")) {
-		promptTemplate += `Voici la température: ${getTemperature(new Date())}°C`
-	}
-	if(question.includes("nouvelles") || question.includes("news")) {
-		promptTemplate += `Voici les news récentes: \n${formatSearchResults(getNewsCache())}`
-	}
+    if (question.includes("météo")) {
+        promptTemplate += `Voici la météo pour les 7 prochains jours: \n${format7DayForecast(getMeteoCache())}. Ne donne que celle qui t'es demandé.`
+    }
+    if (question.includes("température")) {
+        promptTemplate += `Voici la température: ${getTemperature(new Date())}°C`
+    }
+    if (question.includes("nouvelles") || question.includes("news")) {
+        promptTemplate += `Voici les news récentes: \n${formatSearchResults(getNewsCache())}`
+    }
     return promptTemplate.replace(/question/g, question);
 }
 
 function AIMLCallback(answer, wildCardArray, input) {
     const bullshit = getBullshit();
-    return answer && !answer.includes("undefined")
-        ? {
+    return answer && !answer.includes("undefined") ?
+        {
             reply: answer,
             hiragana_form: bullshit.japanese,
             id: bullshit.id,
             type: bullshit.type
-        }
-        : internetDependencyResponses[Math.floor(Math.random() * internetDependencyResponses.length)];
+        } :
+        internetDependencyResponses[Math.floor(Math.random() * internetDependencyResponses.length)];
 }
 
 function getAIMLResponse(text) {
@@ -87,85 +99,91 @@ function getBullshit() {
 }
 
 function formatSearchResults(searchResults) {
-  return searchResults.map(result => {
-    return `${result.Publisher} ${result.PublishedAt} | **${result.Title}** : ${result.Description}`;
-  });
+    return searchResults.map(result => {
+        return `${result.Publisher} ${result.PublishedAt} | **${result.Title}** : ${result.Description}`;
+    });
 }
 
 function convertToHours(publishedAt) {
-  if (publishedAt.includes('heure') || publishedAt.includes('heures')) {
-    return parseInt(publishedAt.match(/\d+/)[0]);
-  } else if (publishedAt.includes('jour') || publishedAt.includes('jours')) {
-    return parseInt(publishedAt.match(/\d+/)[0]) * 24;
-  }
-  return 0;
+    if (publishedAt.includes('heure') || publishedAt.includes('heures')) {
+        return parseInt(publishedAt.match(/\d+/)[0]);
+    } else if (publishedAt.includes('jour') || publishedAt.includes('jours')) {
+        return parseInt(publishedAt.match(/\d+/)[0]) * 24;
+    }
+    return 0;
 }
 
 function sortByRecent(searchResults) {
-  return searchResults.sort((a, b) => {
-    const hoursA = convertToHours(a.PublishedAt);
-    const hoursB = convertToHours(b.PublishedAt);
-    return hoursA - hoursB;
-  });
+    return searchResults.sort((a, b) => {
+        const hoursA = convertToHours(a.PublishedAt);
+        const hoursB = convertToHours(b.PublishedAt);
+        return hoursA - hoursB;
+    });
 }
 
 function getClosestTemperature(date, weatherData) {
-  const targetTime = date.toISOString();
+    const targetTime = date.toISOString();
 
-  let closestIndex = 0;
-  let closestDifference = Math.abs(new Date(weatherData.hourly.time[0]) - date);
+    let closestIndex = 0;
+    let closestDifference = Math.abs(new Date(weatherData.hourly.time[0]) - date);
 
-  for (let i = 1; i < weatherData.hourly.time.length; i++) {
-    const currentDifference = Math.abs(new Date(weatherData.hourly.time[i]) - date);
-    if (currentDifference < closestDifference) {
-      closestDifference = currentDifference;
-      closestIndex = i;
+    for (let i = 1; i < weatherData.hourly.time.length; i++) {
+        const currentDifference = Math.abs(new Date(weatherData.hourly.time[i]) - date);
+        if (currentDifference < closestDifference) {
+            closestDifference = currentDifference;
+            closestIndex = i;
+        }
     }
-  }
 
-  return weatherData.hourly.temperature_2m[closestIndex];
+    return weatherData.hourly.temperature_2m[closestIndex];
 }
 
 function calculateDailyAverages(weatherData) {
-  const dailyAverages = [];
-  let dayTemps = [];
-  let currentDay = new Date(weatherData.hourly.time[0]).getDate();
+    const dailyAverages = [];
+    let dayTemps = [];
+    let currentDay = new Date(weatherData.hourly.time[0]).getDate();
 
-  weatherData.hourly.time.forEach((time, index) => {
-    const date = new Date(time);
-    const day = date.getDate();
+    weatherData.hourly.time.forEach((time, index) => {
+        const date = new Date(time);
+        const day = date.getDate();
 
-    if (day === currentDay) {
-      dayTemps.push(weatherData.hourly.temperature_2m[index]);
-    } else {
-      const averageTemp = dayTemps.reduce((a, b) => a + b, 0) / dayTemps.length;
-      dailyAverages.push({ day: new Date(time).toISOString().split("T")[0], averageTemp });
+        if (day === currentDay) {
+            dayTemps.push(weatherData.hourly.temperature_2m[index]);
+        } else {
+            const averageTemp = dayTemps.reduce((a, b) => a + b, 0) / dayTemps.length;
+            dailyAverages.push({
+                day: new Date(time).toISOString().split("T")[0],
+                averageTemp
+            });
 
-      dayTemps = [weatherData.hourly.temperature_2m[index]];
-      currentDay = day;
+            dayTemps = [weatherData.hourly.temperature_2m[index]];
+            currentDay = day;
+        }
+    });
+
+    if (dayTemps.length > 0) {
+        const averageTemp = dayTemps.reduce((a, b) => a + b, 0) / dayTemps.length;
+        dailyAverages.push({
+            day: new Date(weatherData.hourly.time[weatherData.hourly.time.length - 1]).toISOString().split("T")[0],
+            averageTemp
+        });
     }
-  });
 
-  if (dayTemps.length > 0) {
-    const averageTemp = dayTemps.reduce((a, b) => a + b, 0) / dayTemps.length;
-    dailyAverages.push({ day: new Date(weatherData.hourly.time[weatherData.hourly.time.length - 1]).toISOString().split("T")[0], averageTemp });
-  }
-
-  return dailyAverages;
+    return dailyAverages;
 }
 
 function format7DayForecast(weatherData) {
-  const dailyAverages = calculateDailyAverages(weatherData);
-  
-  const next7DaysForecast = dailyAverages.slice(0, 7).map(dayData => {
-    return `Le ${parseInt(dayData.day.split("-")[2])}, il fera ${parseInt(dayData.averageTemp)}°.`;
-  });
+    const dailyAverages = calculateDailyAverages(weatherData);
 
-  return next7DaysForecast.join("\n");
+    const next7DaysForecast = dailyAverages.slice(0, 7).map(dayData => {
+        return `Le ${parseInt(dayData.day.split("-")[2])}, il fera ${parseInt(dayData.averageTemp)}°.`;
+    });
+
+    return next7DaysForecast.join("\n");
 }
 
 function switchLights(state) {
-	//TO CODE
+    //TO CODE
 }
 
 // API endpoints
@@ -178,31 +196,55 @@ app.post('/mikuAi', async (req, res) => {
         });
     }
 
-    try {
-		const prompt = generatePrompt(content);
-        const { reply } = await herc.question({
-            model: "turbo",
-            content: prompt
-        });
+    const prompt = generatePrompt(content)
 
-        const cleanedReply = reply.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
-        const bullshit = getBullshit();
+    const apiUrl = 'https://api.groq.com/openai/v1/chat/completions';
+    const apiKey = process.env.HERC_API_KEY; // Replace with your actual Groq API key "gsk_..."
+    // const inputPrompt = prompt;
+    const response = await fetch(apiUrl, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${apiKey}`
+        },
+        body: JSON.stringify({
+            "messages": [{
+                "role": "system",
+                "content": prompt
+            }, {
+                "role": "user",
+                "content": content
+            }],
+            "model": "llama3-8b-8192"
+        })
+    }).then(response => {
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`);
+        }
+        return response.json();
+    }).catch(error => {
+        console.error('Error:', error);
+    });
+    const reply = response.choices[0].message.content;
 
-        res.json({
-            success: true,
-            message: {
-                reply: cleanedReply,
-                hiragana_form: bullshit.japanese,
-                id: bullshit.id
-            }
-        });
-    } catch (error) {
-        console.error(error);
-        res.status(500).json({
-            success: false,
-            message: "Internal Server Error"
-        });
-    }
+    const cleanedReply = reply.replace(/([\u2700-\u27BF]|[\uE000-\uF8FF]|\uD83C[\uDC00-\uDFFF]|\uD83D[\uDC00-\uDFFF]|[\u2011-\u26FF]|\uD83E[\uDD10-\uDDFF])/g, '');
+    const bullshit = getBullshit();
+
+    res.json({
+        success: true,
+        message: {
+            reply: cleanedReply,
+            hiragana_form: bullshit.japanese,
+            id: bullshit.id
+        }
+    });
+    // } catch (error) {
+    //     console.error(error);
+    //     res.status(500).json({
+    //         success: false,
+    //         message: "Internal Server Error"
+    //     });
+    // }
 });
 
 
@@ -214,29 +256,34 @@ app.post('/mikuAi-offline', async (req, res) => {
             message: "Content is needed in the JSON payload"
         });
     }
-	
-	let response;
 
-	if(content=="météo") {
-		response = {reply: `Voici la météo pour les 7 prochains jours: \n${format7DayForecast(getMeteoCache())}`}
-	}
-	else if(content=="température") {
-		response = {reply: `La température actuelle est de ${getTemperature(new Date())}°.`}
-	}
-	else if(content=="news" || content=="nouvelles") {
-		response = {reply: `Voici les nouvelles: ${formatSearchResults(getNewsCache())}`}
-	}
-	else if(content=="allume lumière") {
-		switchLights(true);
-		response = {reply: `J'ai allumé la lumière.`}
-	}
-	else if(content=="éteins lumière") {
-		switchLights(false);
-		response = {reply: `J'ai éteint la lumière.`}
-	}
-	else {
-		response = getAIMLResponse(content);		
-	}
+    let response;
+
+    if (content == "météo") {
+        response = {
+            reply: `Voici la météo pour les 7 prochains jours: \n${format7DayForecast(getMeteoCache())}`
+        }
+    } else if (content == "température") {
+        response = {
+            reply: `La température actuelle est de ${getTemperature(new Date())}°.`
+        }
+    } else if (content == "news" || content == "nouvelles") {
+        response = {
+            reply: `Voici les nouvelles: ${formatSearchResults(getNewsCache())}`
+        }
+    } else if (content == "allume lumière") {
+        switchLights(true);
+        response = {
+            reply: `J'ai allumé la lumière.`
+        }
+    } else if (content == "éteins lumière") {
+        switchLights(false);
+        response = {
+            reply: `J'ai éteint la lumière.`
+        }
+    } else {
+        response = getAIMLResponse(content);
+    }
     res.json({
         success: true,
         message: response
@@ -251,7 +298,9 @@ app.post('/voicevox', async (req, res) => {
 
         const response = await fetch(`${VOICEVOX_API_BASE_URL}/synthesis?speaker=${DEFAULT_SPEAKER}`, {
             method: 'POST',
-            headers: { 'Content-Type': 'application/json' },
+            headers: {
+                'Content-Type': 'application/json'
+            },
             body: queryData,
         });
 
@@ -267,18 +316,27 @@ app.post('/voicevox', async (req, res) => {
 
 app.get('/isOnline', async (req, res) => {
     require('dns').resolve('www.google.com', (err) => {
-        res.send({ isOnline: !err });
+        res.send({
+            isOnline: !err
+        });
     });
 });
 
-app.get('/wakeupText', (req,res) => {
-	const wakeupId = req.query?.id;
-	if(!wakeupId || (wakeupId > 6 && wakeupId < 1)) {
-		res.send({success: false})
-	} else {
-		let wakeup = wakeupResponses.find(({id})=>id==wakeupId)
-		res.send({success: true, response: wakeup})
-	}
+app.get('/wakeupText', (req, res) => {
+    const wakeupId = req.query?.id;
+    if (!wakeupId || (wakeupId > 6 && wakeupId < 1)) {
+        res.send({
+            success: false
+        })
+    } else {
+        let wakeup = wakeupResponses.find(({
+            id
+        }) => id == wakeupId)
+        res.send({
+            success: true,
+            response: wakeup
+        })
+    }
 })
 
 // Serve static files
@@ -288,31 +346,35 @@ app.use(express.static(PUBLIC_DIR));
 
 // Start the server
 const httpsOptions = {
-  key: fs.readFileSync('./key.pem'),
-  cert: fs.readFileSync('./cert.pem')
+    key: fs.readFileSync('./key.pem'),
+    cert: fs.readFileSync('./cert.pem')
 }
 const server = https.createServer(httpsOptions, app).listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
 
+// app.listen(PORT, () => {
+//     console.log(`Server running on port ${PORT}`);
+// });
+
 /** APIS */
 //Weather API
-async function getMeteo(latitude,longitude) {	
-	const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&hourly=temperature_2m`;
-	return await fetch(url).then(response => response.json());
+async function getMeteo(latitude, longitude) {
+    const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&current=temperature_2m&hourly=temperature_2m`;
+    return await fetch(url).then(response => response.json());
 }
 
 //News API
-async function getNews(Query){
-    try{
+async function getNews(Query) {
+    try {
         const news = await SearchEngine({
             Method: 'News',
             Page: 1,
             Query,
             Language: 'fr'
         });
-		return sortByRecent(news.Results);
-    }catch(SearchRuntimeError){
+        return sortByRecent(news.Results);
+    } catch (SearchRuntimeError) {
         console.log(SearchRuntimeError);
     }
 }
@@ -320,55 +382,62 @@ async function getNews(Query){
 /** Get caches */
 // Weather cache
 function getMeteoCache() {
-	return JSON.parse(fs.readFileSync("./cache/weather.json").toString());
+    return JSON.parse(fs.readFileSync("./cache/weather.json").toString());
 }
 
 // News cache
 function getNewsCache() {
-	return JSON.parse(fs.readFileSync("./cache/news.json").toString());
+    return JSON.parse(fs.readFileSync("./cache/news.json").toString());
 }
 
 /** Get caches specific values */
 // Weather value
 function getTemperature(date) {
-	const meteo = getClosestTemperature(date, getMeteoCache());
-	return meteo;
+    const meteo = getClosestTemperature(date, getMeteoCache());
+    return meteo;
 }
 
 /** Caches daemons */
 //Weather cache
-async function weatherDaemon(){	
-	let data = await getMeteo(process.env.LATITUDE,process.env.LONGITUDE)
-	fs.writeFileSync("./cache/weather.json", JSON.stringify(data,0,2));
+async function weatherDaemon() {
+    let data = await getMeteo(process.env.LATITUDE, process.env.LONGITUDE)
+    fs.writeFileSync("./cache/weather.json", JSON.stringify(data, 0, 2));
 }
 
 //News cache
-async function newsDaemon(){	
-	let data = await getNews(process.env.NEWS_PREFERENCES)
-	fs.writeFileSync("./cache/news.json", JSON.stringify(data,0,2));
+async function newsDaemon() {
+    let data = await getNews(process.env.NEWS_PREFERENCES)
+    fs.writeFileSync("./cache/news.json", JSON.stringify(data, 0, 2));
 }
 
 //Daemons every hour
 function executeDaemons() {
-	require('dns').resolve('www.google.com', async (err) => {
-        if(!err) {
-			await weatherDaemon();
-			await newsDaemon();
-		}
+    require('dns').resolve('www.google.com', async (err) => {
+        if (!err) {
+            await weatherDaemon();
+            await newsDaemon();
+        }
     });
 }
-setInterval(executeDaemons, 60 * 60 * 1000);	
+setInterval(executeDaemons, 60 * 60 * 1000);
 
 //First launch
 executeDaemons();
 
 //Prevent process from crashing
-process.on('uncaughtException', (error)  => {
-    console.log('Oh my god, something terrible happened: ',  error);
+process.on('uncaughtException', (error) => {
+    console.log('Oh my god, something terrible happened: ', error);
     process.exit(1); // exit application 
 })
 
 process.on('unhandledRejection', (error, promise) => {
-  console.log(' Oh Lord! We forgot to handle a promise rejection here: ', promise);
-  console.log(' The error was: ', error );
+    console.log(' Oh Lord! We forgot to handle a promise rejection here: ', promise);
+    console.log(' The error was: ', error);
 });
+// This is just a sample script. Paste your real code (javascript or HTML) here.
+
+if ('this_is' == /an_example/) {
+    of_beautifier();
+} else {
+    var a = b ? (c % d) : e[f];
+}
